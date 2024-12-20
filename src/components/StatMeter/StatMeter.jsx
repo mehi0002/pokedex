acaab8498685c3968effe71122df2d0bf0ed4db0
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2024-12-19 20:56:41 -0500

    Added base stat meters

--- src/components/StatMeter/StatMeter.jsx
+++ src/components/StatMeter/StatMeter.jsx
@@ -1,9 +1,12 @@
 import './StatMeter.css';
 
-function StatMeter() {
+// Displays a meter representing the value of a pokemon's base stat
+function StatMeter(props) {
 
   return (
     <>
+      <label for={`${props.pokeName}_${props.statName}`}> {props.statName} </label>
+      <meter min='0' max='255' value={props.base}></meter>
     </>
   );
 }
--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,15 +1,31 @@
 import './Stats.css';
 import Type from '../Type/Type';
+import StatMeter from '../StatMeter/StatMeter';
 
 // Displays a pokemon's stats
 function Stats(props) {
+  
   return (
-    <div className="stats">
-      <Type> {props.poke.types[0].type.name} </Type>
-      {
-        props.poke.types[1] &&
-        <Type> {props.poke.types[1].type.name} </Type>
-      }
+    <div className='stats'>
+
+      <div className="statBlock">
+        <Type> {props.poke.types[0].type.name} </Type>            {/* Type #1 */}            
+        { props.poke.types[1] && 
+          <Type> {props.poke.types[1].type.name} </Type>          /* Type #2 */
+        }
+        <p>{`Height: ${props.poke.height}`}</p>                   {/* Height */}
+        <p>{`Weight: ${props.poke.weight}`}</p>                   {/* Weight */}
+        <p>{`Base XP: ${props.poke.base_experience}`}</p>         {/* Base XP */}
+      </div>
+
+      <div className='statMeters'>                                {/* Base stats */}
+        {
+          props.poke.stats.map( stat =>
+            <StatMeter pokeName={props.poke.name} statName={stat.stat.name} base={stat.base_stat} />
+          )
+        }
+      </div>
+
     </div>
   );
 }
