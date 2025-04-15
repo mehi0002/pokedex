5d8dfc3f9688b644f61a8dba18c33a258c699706
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2025-04-15 13:06:40 -0400

    Gallery - more button

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,21 +2,20 @@ import PokemonCard from '../PokemonCard';
 import './Gallery.css';
 
 // Displays a list of pokemon info
-// Displays stats when a pokemon is selected
 function Gallery({pokemon, caught, catchHandler}) {
 
   /**** Build *****/
   return (
-    // <article>
-      <ul id='pokemonGallery'>
+    <article id='pokemonGallery'>
+      <ul>
         {pokemon.map( (poke, index) => 
           <li key={index+1}>
-            {/* <PokemonCard poke={poke} caught={caughtList[index]} catchHandler={catchHandler} /> */}
             <PokemonCard name={poke.name} url={poke.url} caught={ caught[poke.name] ? true : false } catchHandler={catchHandler} />
           </li>
         )}
       </ul>
-    // </article>
+      <button>Load More</button>
+    </article>
   );
 }
 
