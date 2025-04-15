0d20f42f4517cc69b590e05007278d91855df701
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2025-04-15 12:23:59 -0400

    Chnaged app.jsx to using an outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
-import { useState, useEffect } from 'react';
-import Gallery from './components/Gallery'
-import './App.css';
+import { Outlet } from "react-router-dom";
 
 function App() {
-
-  /*** States ***/
-  const [pokemon, setPokemon] = useState([]);
-  const [caught, setCaught] = useLocalStorage('caught', {});
-
-  /*** Effects ***/
-
-  // Fetching the first 20 pokemon from the Poke API
-  useEffect(() => {                                   
-    fetch('https://pokeapi.co/api/v2/pokemon')
-    .then(response => response.json())
-    .then(json => {setPokemon(json.results); console.log('getting first 20 pokemon...')} )
-    }, [] );
-
-  /*** Functions ***/
-
-  function useLocalStorage(key, defaultValue){
-    const obj = localStorage.getItem(key);
-    return( useState( obj ? obj : defaultValue));
-  }
-  
-  /*** Build ***/
-  return (
-    <main className="container-fluid">
-      {pokemon[0] ?
-        <Gallery pokemon={pokemon} caught={caught}/> :
-        <p>loading...</p>
-      }
-    </main>
-  );
+  return(
+    <Outlet></Outlet>
+  )
 }
 
 export default App
+
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,7 @@ function Home (){
 
     /*** States ***/
     const [pokemon, setPokemon] = useState([]);
-    const [caughtList, setCaughtList] = useState([]);
+    const [caught, setCaught] = useLocalStorage('caught', {});
 
     /*** Effects ***/
 
@@ -16,23 +16,29 @@ function Home (){
         .then(json => {setPokemon(json.results); console.log('getting first 20 pokemon...')} )
         }, [] );
 
+    /*** Functions ***/
+    function useLocalStorage(key, defaultValue){
+        const obj = localStorage.getItem(key);
+        return( useState( obj ? obj : defaultValue));
+      }
+    
     /*** Handlers ***/
 
     // Toggles the caught status and updates the pokemon state
     function toggleCaughtHandler(){           
-        console.log(`setting caught status to true...`)
+        console.log(`toggling caught status...`)
     }
 
     /*** Build ***/
     return (
         <main className="container-fluid">
-        {console.log(pokemon)}
-        {pokemon[0] ?
-            <Gallery pokemon={pokemon} caughtList={caughtList} catchHandler={toggleCaughtHandler}/> :
+            {console.log(pokemon)}
+            { pokemon[0] ?
+            <Gallery pokemon={pokemon} caught={caught}/> :
             <p>loading...</p>
-        }
+            }
         </main>
-    );
+      );
 }
 
 export default Home;
