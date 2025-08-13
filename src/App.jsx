186634ca88b09b967379a924e3b45fc49b19f1af
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2025-08-13 10:12:15 -0400

    Org - Took out react router and redundant files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,177 @@
-import { Outlet} from "react-router-dom";
+import { useState, useEffect} from 'react';
+import Gallery from './components/Gallery';
+import PokemonInfo  from './components/PokemonInfo';
+import CaughtList from './components/CaughtList';
 
 function App() {
+
+  /****** States ******/
+  const [pokemonList, setpokemonList] = useState([]);
+  const [url, setURL] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [pokemon, setPokemon] = useState([]);
+  const [caughtList, setCaughtList] = useLocalStorage('caught', []);
+  const [selectedPoke, setSelectedPoke] = useState('');
+
+  /****** Effects ******/
+
+  // Fetch first 20 pokemon on load
+  useEffect(() => { 
+    fetchPokemon();   
+  }, [] );
+
+  //Save caught list to local storage when updated
+  useEffect( () => {
+    localStorage.setItem('caught', JSON.stringify(caughtList) );
+  }, [caughtList] );
+
+  /****** Functions ******/
+
+  // Initialize state with local storage
+  function useLocalStorage(key, defaultValue){
+    const obj = localStorage.getItem(key);
+    return( useState( obj ? JSON.parse(obj) : defaultValue));
+    }
+
+  // Fetch the next set of pokemon and their info from the PokeAPI
+  async function fetchPokemon(){
+    const listResults = [];
+    const pokeURLs = [];
+    const pokeList = []
+
+    // Get the list of pokemon names and urls
+    await fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        listResults.push(...json.results);
+        setpokemonList([...pokemonList, ...json.results]);
+        setURL(json.next);
+    })
+    .catch(error => console.log(`error getting pokemon names and urls: ${error}`));
+
+    listResults.map(poke => pokeURLs.push(poke.url))
+
+    // Get the info for each pokemon
+    const promiseList = pokeURLs.map(pokeURL => fetch(pokeURL));
+
+    await Promise.allSettled(promiseList)
+    .then(responses => Promise.allSettled( responses.map(response => response.value.json())))
+    .then(data => data.map(poke => pokeList.push(poke.value)))
+    .catch(error => console.log(`Error fetching pokemon info: ${error}`));
+
+    // Add to the current list
+    setPokemon([...pokemon, ...pokeList])
+  }
+
+  function closePokeInfo(){
+    document.getElementById("pokeInfo").classList.remove("view");
+  }
+
+  /****** Handlers *******/
+
+  // Toggles the caught status and updates the pokemon state
+  function toggleCaughtHandler(pokeName){  
+    const selected = pokemon.find(poke => poke.name == pokeName);
+    const caught = caughtList.find(poke => poke.name == pokeName);
+    const tempList = [...caughtList];
+
+    // If already caught, remove from caught list
+    if(caught)
+      setCaughtList( caughtList.filter(caughtPoke => caughtPoke.name != pokeName) ) 
+    
+    // If not yet caught, add the pokemon name & sprite url to the caught list
+    else {
+      tempList.push( {name:selected.name, sprite:selected['sprites']['front_default']} );
+      
+      tempList.sort( (a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        
+        if (nameA < nameB) 
+          return-1;
+        else if (nameA > nameB)
+          return 1;
+        else
+          return 0;
+      });
+
+      setCaughtList( [...tempList] );
+    }
+  }
+
+  //Display pokemon when card is clicked
+  function selectPokeHandler(selected){
+    const newPoke = pokemon.find(poke => poke.name == selected);
+    setSelectedPoke({...newPoke});
+
+    document.getElementById("caughtList").firstElementChild.removeAttribute("open");
+    document.getElementById("pokeInfo").classList.add("view");
+  }
+
+  /*** Build ***/
   return(
-    <>
     <div id="siteContainer">
       <header id="siteHeader"> 
-          <h1>Pokedex</h1> 
+        <h1>Pokedex</h1> 
       </header>
 
-        <Outlet></Outlet>
+      <div id="homeContent">
+
+        <div id="sidebar">
+          {/* Caught List */}
+          { pokemon[0] ?
+            <aside id="caughtList" className="container">
+                <details>
+                    <summary><h2>Caught List</h2></summary>
+                    <CaughtList caughtList={caughtList} catchHandler={toggleCaughtHandler} />
+                </details>
+            </aside>
+          :
+            <p>loading...</p>
+          }
+
+          {/* Pokemon info display */}
+          <aside id="pokeInfo">
+            { selectedPoke &&
+              <>
+                <PokemonInfo poke = {selectedPoke} />
+                <button 
+                  id="closePokeInfo" 
+                  className="container" 
+                  aria-label="close" 
+                  onClick={closePokeInfo}
+                >
+                  X
+                </button>
+              </>
+            }
+          </aside>
+
+        </div>
+
+        {/* Main Gallery */}
+        { pokemon[0] ?
+          <main id="mainContent">
+            {/* Pokemon List */}
+            <Gallery 
+              pokemonList={pokemon} 
+              caught={caughtList}
+              catchHandler={toggleCaughtHandler}
+              selectPokeHandler = {selectPokeHandler}
+            /> 
+            <button 
+              id="loadMore" 
+              className="container" 
+              aria-label="load more pokemon" 
+              onClick={fetchPokemon}
+            >
+              Load More
+            </button>
+          </main>
+          :
+          <p>loading...</p>
+        }
+      </div>
     </div>
-    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,32 @@
 import { StrictMode } from 'react';
 import { createRoot} from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import App from './App.jsx';
-import Home from './routes/Home.jsx';
-import Info from './routes/Info.jsx';
-import Caught from './routes/Caught.jsx';
+// import Home from './routes/Home.jsx';
+// import Info from './routes/Info.jsx';
+// import Caught from './routes/Caught.jsx';
 
-const router = createBrowserRouter([
-  { path: '/', 
-    element: <App/>,
-    children: [
-      { path: '/', element: <Home/> },
-      { path: '/:pokeName', element: <Info/>},
-      { path: '/caught', element: <Caught/>}
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   { path: '/', 
+//     element: <App/>,
+//     children: [
+//       { path: '/', element: <Home/> },
+//       { path: '/:pokeName', element: <Info/>},
+//       { path: '/caught', element: <Caught/>}
+//     ]
+//   }
+// ]);
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>
+// )
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-)
+    <App />
+  </StrictMode>,
+);
